b6b8589576e23616e9a58f742b9060136992dbe2
Ailish Stein <as118516@ohio.edu>
2020-01-21 10:48:34 -0500

    Adding Program

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Ailish Stein	
+ *        Date: January 21st 2020
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout <<"Hello Git!!"<< endl;
+    /*add code*/
+    return 0;
+}// main
